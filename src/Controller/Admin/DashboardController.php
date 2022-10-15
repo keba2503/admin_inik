a5a4c3c5af7ed3a1d44ba92a481acbd48d5e0f40
Karen Borrero <keba2503@gmail.com>
2022-10-15 20:47:32 +0100

    eliminar user de dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Controller\Admin;
 
 
-use App\Entity\User;
+
 use App\Entity\Vehiculos;
 use App\Entity\Images;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
@@ -19,7 +19,7 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         $routeBuilder = $this->container->get(AdminUrlGenerator::class);
-                $url = $routeBuilder->setController(UserCrudController::class)->generateUrl();
+                $url = $routeBuilder->setController(VehiculosCrudController::class)->generateUrl();
         
                 return $this->redirect($url);
       
@@ -34,7 +34,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoRoute('Inicio', 'fas fa-home', 'admin');
-        yield MenuItem::linkToCrud('User', 'fas fa-map', User::class);
         yield MenuItem::linkToCrud('Vehiculos', 'fas fa-shuttle-van', Vehiculos::class);
         yield MenuItem::linkToCrud('Images', 'fa fa-file-image-o', Images::class);
     }
